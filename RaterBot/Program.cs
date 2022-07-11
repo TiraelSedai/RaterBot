39b798866dec5bf81c988d6bda9540cff6b20804
Evgenii Burmistrov <me@burm.dev>
2022-07-11 10:02:01 +0300

    Csharpier to Program.cs

--- RaterBot/Program.cs
+++ RaterBot/Program.cs
@@ -1,9 +1,17 @@
 using RaterBot;
+using Telegram.Bot;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+        services.AddSingleton<ITelegramBotClient>(
+            _ =>
+                new TelegramBotClient(
+                    Environment.GetEnvironmentVariable("TELEGRAM_MEDIA_RATER_BOT_API")
+                        ?? throw new Exception("TELEGRAM_MEDIA_RATER_BOT_API environment variable not set")
+                )
+        );
     })
     .Build();
 
